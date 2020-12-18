a5bbb530e1e56e76183a5b73bfc564bc90923bfc
Braxton Shoop <braxton.shoop@gmail.com>
2020-12-17 20:25:11 -0600

    baseline functionality established

---
+++ Palindrome Checker/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Palindrome_Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter any word here to see if it is a palindrome:  ");
+                string input = Console.ReadLine();
+                string reverseInput = "";
+                string response = "";
+
+                for (int i = input.Length - 1; i >= 0; i--)
+                {
+                    reverseInput += input[i].ToString();
+                }
+                Console.WriteLine("-------------------------\n");
+                Console.WriteLine($"That word backwards is spelled {reverseInput}\n");
+
+                if (input == reverseInput)
+                {
+                    Console.WriteLine("That word is a palindrome.\n");
+
+                }
+                else
+                {
+                    Console.WriteLine("That word is not a palindrome.\n");
+                }
+
+                Console.Write("Want to check another word? (Enter Y or N):  \n");
+                Console.WriteLine();
+                response = Console.ReadLine();
+                Console.WriteLine();
+                if (response.ToLower() == "y")
+                {
+
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
